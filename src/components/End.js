acee07da105f655bf853795d23c9f8729bb55e29
MARQUESES <giamarqueses@gmail.com>
2021-07-18 14:05:08 +0800

    change

--- src/components/End.js
+++ src/components/End.js
@@ -4,8 +4,19 @@ import { Segment, Header, Divider } from "semantic-ui-react";
 const End = ({ song }) => {
   return (
     <div>
-      <button style={{ width: "70%", margin: "0.5em", padding: "1em" }}>
-        <Header as="h5">{song}</Header>
+      <button
+        style={{
+          width: "70%",
+          margin: "0.5em",
+          padding: "1em",
+          backgroundColor: "#c020fa",
+          border: "none",
+          outline: "none",
+        }}
+      >
+        <Header as="h5" style={{ color: "white" }}>
+          {song}
+        </Header>
       </button>
     </div>
   );
--- src/components/Op.js
+++ src/components/Op.js
@@ -3,7 +3,13 @@ import { Segment, Header, Divider } from "semantic-ui-react";
 const Op = ({ song }) => {
   return (
       <div>
-     <button style={{ width: "70%", margin: "0.5em", padding: "1em" ,backgroundColor:'#c020fa'}}>
+          <button style={{
+              width: "70%",
+              margin: "0.5em",
+              padding: "1em",
+              backgroundColor: '#c020fa',
+              border: "none",
+          outline: "none",}}>
         <Header as='h5' style={{ color:'white'}}>{song}</Header>
       </button>
     </div>
